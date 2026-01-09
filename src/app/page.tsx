1013fad75306712eccb30b98d798841fe5ba2697
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2026-01-09 23:23:15 +0530

    Add Problem component with styles; integrate into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/features/home/hero";
+import Problem from "@/features/home/problem";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Problem />
     </>
   );
 }
