19f8389c46793390e48d06a8ed8c61ffca045dcd
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2026-01-21 23:29:04 +0530

    fix: remove Google Analytics import and error check; streamline Google TagManager setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 import Navbar from "@/components/shared/navbar";
 import Footer from "@/components/shared/footer";
 import GSAPInit from "@/components/shared/gsap-init";
 import { Toaster } from "sonner";
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID as string;
 const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID as string;
-if (!GA_ID && !GTM_ID) {
-  throw new Error("Google Analytics/TagManager ID not provided.");
+if (!GTM_ID) {
+  throw new Error("Google TagManager ID not provided.");
 }
 
 export default function RootLayout({
@@ -27,7 +26,6 @@ export default function RootLayout({
         <GSAPInit />
         <Toaster position="bottom-right" richColors />
       </body>
-      {/* <GoogleAnalytics gaId="G-1Q5WZHE67B" /> */}
       <GoogleTagManager gtmId={GTM_ID} />
     </html>
   );
